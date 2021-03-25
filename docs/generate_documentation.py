71a5f35fe8f23874f25cabf577bcf77ac9e13aa3
Ziv Yaniv <zivyaniv@nih.gov>
2021-03-25 14:17:40 -0400

    Updating website generation code.

--- docs/generate_documentation.py
+++ docs/generate_documentation.py
@@ -14,11 +14,24 @@ extensions_information = [
     ]
 ]
 
+# Create the index.html file, make the urls relative so
+# website is self contained (in the README.md they are
+# absolute as the website is on a different branch).
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 os.system(
     f'pandoc -s ../README.md -o index.html -c {css_file_name} --metadata title="SimpleITK Imaris Extensions"'
 )
+url_prefix = "http://niaid.github.io/imaris_extensions/"
+with open("index.html", "r") as fp:
+    index_html = fp.read()
+for f, a in extensions_information:
+    local_url = os.path.split(f)[1][:-3] + ".html"
+    index_html = index_html.replace(url_prefix + local_url, local_url, 1)
+with open("index.html", "w") as fp:
+    fp.write(index_html)
 
+
+# Create the html files for all extensions.
 all_paths = set([os.path.dirname(d) for d, a in extensions_information])
 for p in all_paths:
     sys.path.append(p)
